8a2ec0a4d698e85454c93d8cb6c6b1b8767fc384
DustarMao <DustarMao@users.noreply.github.com>
2021-02-09 07:00:57 +0000

    deploy: e882ae84dcb29be3a6091a5ea2dbb252a74ee893

---
+++ _next/static/lKGz057Pd1XBz8TH6Foav/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Faddf4301b1a2bf820d11.css","static\u002Fchunks\u002Fpages\u002Findex-a0e26214ffe9959fcee3.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-312cb4bdf967fd9d5ef6.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
